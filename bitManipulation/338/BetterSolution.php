dd1d333bf1e2a8451f0c0ab304201239866c3660
Shun Inagaki <s0i6n9g6k@gmail.com>
2023-11-30 08:52:42 +0900

    338:Solution and Another Solution

---
+++ bitManipulation/338/BetterSolution.php
@@ -0,0 +1,20 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer $n
+     * @return Integer[]
+     */
+    function countBits($n) {
+        $dp = [0];
+        $offset = 1;
+        for ($i=1; $i<=$n; $i++){
+            if($i == $offset * 2){
+                $offset = $i;
+            }
+            $dp[$i]= 1 + $dp[$i - $offset];
+        }
+        return $dp;
+    }
+}
